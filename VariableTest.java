b206e2dc04cad1e8c63d8bb5dca2dd5980a2d588
Dhiraj093328 <162727053+Dhiraj093328@users.noreply.github.com>
2025-09-24 11:49:02 +0530

    VariableTest java program

---
+++ VariableTest.java
@@ -0,0 +1,30 @@
+class VariableTest{
+
+	public static void main(String args[])
+	{
+
+		byte a=1;
+		short b=5;
+		int no=10;
+		long d=1001;
+		Boolean status=true;
+		float f=1.5f;
+		double df=13.5;
+		char c='s';
+
+
+
+
+		System.out.println("value of byte: "+a);
+		System.out.println("value of short: "+b);
+		System.out.println("value of int: "+no);
+		System.out.println("value of long: "+d);
+		System.out.println("value of boolean: "+status);
+		System.out.println("value of float: "+f);
+		System.out.println("value of char: "+c);
+		System.out.println("value of double: "+df);
+
+	
+	}
+
+}
