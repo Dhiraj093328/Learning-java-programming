78eab16b9332ee354237c61721039b5759ba2301
Dhiraj093328 <162727053+Dhiraj093328@users.noreply.github.com>
2025-11-10 11:53:44 +0530

    todays

---
+++ StringImmutability.java
@@ -0,0 +1,16 @@
+class StringImmutability
+{
+
+	public static void main(String args[])
+	{
+		String s1="Hello";
+
+
+		System.out.println(s1.concat(" Minal "));
+
+		System.out.println(s1);
+
+
+	}
+
+}
