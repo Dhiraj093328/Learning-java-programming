f13441e0753e654f06b3e90a2eb8b9860b50b003
Dhiraj093328 <162727053+Dhiraj093328@users.noreply.github.com>
2025-11-30 22:03:09 +0530

    Daemon Thread

---
+++ DaemonThreadDemo.java
@@ -0,0 +1,30 @@
+class DaemonThreadDemo extends Thread
+{
+	public void run()
+	{
+		//Identify thread is Daemon or not
+		if(Thread.currentThread().isDaemon())
+		{
+		   System.out.println("Daemon Thread executed");
+		}
+		else
+		{
+
+		   System.out.println("user Threaxd executed");
+		}
+	}
+
+	public static void main(String args[])
+	{
+		DaemonThreadDemo t1=new DaemonThreadDemo();
+		DaemonThreadDemo t2=new DaemonThreadDemo();
+		DaemonThreadDemo t3=new DaemonThreadDemo();
+
+		t1.setDaemon(true);
+
+		t1.start();
+		t2.start();
+		t3.start();
+
+	}
+}
