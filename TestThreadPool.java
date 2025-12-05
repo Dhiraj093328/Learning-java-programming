4f3dada8bb2a0178e572cbe61da4bf8b89c40e63
Dhiraj093328 <162727053+Dhiraj093328@users.noreply.github.com>
2025-12-05 10:10:24 +0530

    threadpool

--- TestThreadPool.java
+++ TestThreadPool.java
@@ -7,7 +7,7 @@ class TestThreadPool
 	{
         	ExecutorService executor = Executors.newFixedThreadPool(5);
 
-        	for (int i = 0; i < 10; i++)
+        	for (int i = 0; i < 20; i++)
 		{
             		Runnable worker = new WorkerThread("" + i);
             		executor.execute(worker);  // correct method call
